990db9c8ae1ed8bcec06417e9a988e2090a6c97b
Uchchash346 <mru416@gmail.com>
2021-02-02 19:24:44 +0600

    first commit

--- undefined.js
+++ undefined.js
@@ -1,4 +1,7 @@
-//let pakhi;
+/*let pakhi;
+console.log(pakhi)*/
+
+
 
 /*function add(num1, num2){
     console.log(num1 + num2);
@@ -11,5 +14,7 @@ console.log(result);*/
 /*let fun = undefined;
 console.log(fun);*/
 
-let ages = [2,5,9];
-console.log(ages[11]);
+/*let ages = [2,5,9];
+console.log(ages[11]);*/
+var foo = null;
+console.log(foo);
