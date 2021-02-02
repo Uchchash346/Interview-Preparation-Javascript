6f69a3d0f50e181b234b2f416e436bb8c6d15d99
Uchchash346 <mru416@gmail.com>
2021-02-02 18:10:43 +0600

    first commit

---
+++ array-split.js
@@ -0,0 +1,11 @@
+const nums = [2,3,4,5,6,7,8,9];
+//const part = nums.slice(2, 5)
+
+//const remove = nums.splice(2,3);
+const remove = nums.splice(2,3,77);
+//console.log(remove);
+//console.log(nums);
+//console.log(part);
+
+const together = nums.join("ami")
+console.log(together);
