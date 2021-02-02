6c02683115ef88d8182f6a00e263588365c0b303
Uchchash346 <mru416@gmail.com>
2021-02-02 13:51:30 +0600

    first commit

---
+++ object-property.js
@@ -0,0 +1,13 @@
+const students = [
+    {id: 21, name:'Omar Sunny'},
+    {id: 31, name:'Mannaa'},
+    {id: 41, name:'Moyuri'},
+    {id: 71, name:'DeepJol'},
+];
+
+const names = students.map(s => s.name);
+const idS = students.map(s => s.id);
+// Problem?
+const bigger = students.filter(s => s.id>40);
+const biggerOne = students.find(s => s.id>40);
+console.log(biggerOne);
